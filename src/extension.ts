f0d72fff72c6ec5fb8e921560b380dfceb856e46
Robert Schili <rschili@users.noreply.github.com>
2025-03-17 12:47:46 +0100

    Add completionprovider and codeactionsprovider from examples

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,7 @@
 import * as vscode from 'vscode';
+import { subscribeToDocumentChanges, EMOJI_MENTION } from './diagnostics';
+
+const COMMAND = 'code-actions-sample.command';
 
 const tokenTypes = new Map<string, number>();
 const tokenModifiers = new Map<string, number>();
@@ -21,7 +24,95 @@ const legend = (function() {
 })();
 
 export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push(vscode.languages.registerDocumentSemanticTokensProvider({ language: 'xml', pattern: '**/*.ecschema.xml' }, new DocumentSemanticTokensProvider(), legend));
+	context.subscriptions.push(
+		vscode.languages.registerDocumentSemanticTokensProvider({ language: 'xml', pattern: '**/*.ecschema.xml' }
+			, new DocumentSemanticTokensProvider(), legend));
+	context.subscriptions.push(
+		vscode.languages.registerCodeActionsProvider('xml', new Emojizer(), {
+			providedCodeActionKinds: Emojizer.providedCodeActionKinds
+		}));
+
+
+	const emojiDiagnostics = vscode.languages.createDiagnosticCollection("emoji");
+	context.subscriptions.push(emojiDiagnostics);
+
+	subscribeToDocumentChanges(context, emojiDiagnostics);
+
+	context.subscriptions.push(
+		vscode.languages.registerCodeActionsProvider('markdown', new Emojinfo(), {
+			providedCodeActionKinds: Emojinfo.providedCodeActionKinds
+		})
+	);
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand(COMMAND, () => vscode.env.openExternal(vscode.Uri.parse('https://unicode.org/emoji/charts-12.0/full-emoji-list.html')))
+	);
+
+	const provider1 = vscode.languages.registerCompletionItemProvider('plaintext', {
+
+		provideCompletionItems(_document: vscode.TextDocument, _position: vscode.Position, _token: vscode.CancellationToken, _context: vscode.CompletionContext) {
+
+			// a simple completion item which inserts `Hello World!`
+			const simpleCompletion = new vscode.CompletionItem('Hello World!');
+
+			// a completion item that inserts its text as snippet,
+			// the `insertText`-property is a `SnippetString` which will be
+			// honored by the editor.
+			const snippetCompletion = new vscode.CompletionItem('Good part of the day');
+			snippetCompletion.insertText = new vscode.SnippetString('Good ${1|morning,afternoon,evening|}. It is ${1}, right?');
+			const docs = new vscode.MarkdownString("Inserts a snippet that lets you select [link](x.ts).");
+			snippetCompletion.documentation = docs;
+			docs.baseUri = vscode.Uri.parse('http://example.com/a/b/c/');
+
+			// a completion item that can be accepted by a commit character,
+			// the `commitCharacters`-property is set which means that the completion will
+			// be inserted and then the character will be typed.
+			const commitCharacterCompletion = new vscode.CompletionItem('console');
+			commitCharacterCompletion.commitCharacters = ['.'];
+			commitCharacterCompletion.documentation = new vscode.MarkdownString('Press `.` to get `console.`');
+
+			// a completion item that retriggers IntelliSense when being accepted,
+			// the `command`-property is set which the editor will execute after 
+			// completion has been inserted. Also, the `insertText` is set so that 
+			// a space is inserted after `new`
+			const commandCompletion = new vscode.CompletionItem('new');
+			commandCompletion.kind = vscode.CompletionItemKind.Keyword;
+			commandCompletion.insertText = 'new ';
+			commandCompletion.command = { command: 'editor.action.triggerSuggest', title: 'Re-trigger completions...' };
+
+			// return all completion items as array
+			return [
+				simpleCompletion,
+				snippetCompletion,
+				commitCharacterCompletion,
+				commandCompletion
+			];
+		}
+	});
+
+	const provider2 = vscode.languages.registerCompletionItemProvider(
+		'plaintext',
+		{
+			provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
+
+				// get all text until the `position` and check if it reads `console.`
+				// and if so then complete if `log`, `warn`, and `error`
+				const linePrefix = document.lineAt(position).text.slice(0, position.character);
+				if (!linePrefix.endsWith('console.')) {
+					return undefined;
+				}
+
+				return [
+					new vscode.CompletionItem('log', vscode.CompletionItemKind.Method),
+					new vscode.CompletionItem('warn', vscode.CompletionItemKind.Method),
+					new vscode.CompletionItem('error', vscode.CompletionItemKind.Method),
+				];
+			}
+		},
+		'.' // triggered whenever a '.' is being typed
+	);
+
+	context.subscriptions.push(provider1, provider2);
 }
 
 interface IParsedToken {
@@ -99,5 +190,84 @@ class DocumentSemanticTokensProvider implements vscode.DocumentSemanticTokensPro
 			tokenType: parts[0],
 			tokenModifiers: parts.slice(1)
 		};
+	}
+}
+
+
+/**
+ * Provides code actions for converting :) to a smiley emoji.
+ */
+export class Emojizer implements vscode.CodeActionProvider {
+
+	public static readonly providedCodeActionKinds = [
+		vscode.CodeActionKind.QuickFix
+	];
+
+	public provideCodeActions(document: vscode.TextDocument, range: vscode.Range): vscode.CodeAction[] | undefined {
+		if (!this.isAtStartOfSmiley(document, range)) {
+			return;
+		}
+
+		const replaceWithSmileyCatFix = this.createFix(document, range, '😺');
+
+		const replaceWithSmileyFix = this.createFix(document, range, '😀');
+		// Marking a single fix as `preferred` means that users can apply it with a
+		// single keyboard shortcut using the `Auto Fix` command.
+		replaceWithSmileyFix.isPreferred = true;
+
+		const replaceWithSmileyHankyFix = this.createFix(document, range, '💩');
+
+		const commandAction = this.createCommand();
+
+		return [
+			replaceWithSmileyCatFix,
+			replaceWithSmileyFix,
+			replaceWithSmileyHankyFix,
+			commandAction
+		];
+	}
+
+	private isAtStartOfSmiley(document: vscode.TextDocument, range: vscode.Range) {
+		const start = range.start;
+		const line = document.lineAt(start.line);
+		return line.text[start.character] === ':' && line.text[start.character + 1] === ')';
+	}
+
+	private createFix(document: vscode.TextDocument, range: vscode.Range, emoji: string): vscode.CodeAction {
+		const fix = new vscode.CodeAction(`Convert to ${emoji}`, vscode.CodeActionKind.QuickFix);
+		fix.edit = new vscode.WorkspaceEdit();
+		fix.edit.replace(document.uri, new vscode.Range(range.start, range.start.translate(0, 2)), emoji);
+		return fix;
+	}
+
+	private createCommand(): vscode.CodeAction {
+		const action = new vscode.CodeAction('Learn more...', vscode.CodeActionKind.Empty);
+		action.command = { command: COMMAND, title: 'Learn more about emojis', tooltip: 'This will open the unicode emoji page.' };
+		return action;
+	}
+}
+
+/**
+ * Provides code actions corresponding to diagnostic problems.
+ */
+export class Emojinfo implements vscode.CodeActionProvider {
+
+	public static readonly providedCodeActionKinds = [
+		vscode.CodeActionKind.QuickFix
+	];
+
+	provideCodeActions(_document: vscode.TextDocument, _range: vscode.Range | vscode.Selection, context: vscode.CodeActionContext, _token: vscode.CancellationToken): vscode.CodeAction[] {
+		// for each diagnostic entry that has the matching `code`, create a code action command
+		return context.diagnostics
+			.filter(diagnostic => diagnostic.code === EMOJI_MENTION)
+			.map(diagnostic => this.createCommandCodeAction(diagnostic));
+	}
+
+	private createCommandCodeAction(diagnostic: vscode.Diagnostic): vscode.CodeAction {
+		const action = new vscode.CodeAction('Learn more...', vscode.CodeActionKind.QuickFix);
+		action.command = { command: COMMAND, title: 'Learn more about emojis', tooltip: 'This will open the unicode emoji page.' };
+		action.diagnostics = [diagnostic];
+		action.isPreferred = true;
+		return action;
 	}
 }
