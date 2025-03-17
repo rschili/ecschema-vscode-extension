4be6703369fa4d8ca68bc6ba853bf3e773e9854a
Robert Schili <rschili@users.noreply.github.com>
2025-03-17 16:32:47 +0100

    store before refactoring to ecschema

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,13 @@
 import * as vscode from 'vscode';
-import { subscribeToDocumentChanges, EMOJI_MENTION } from './diagnostics';
 import * as path from 'path';
 
 const COMMAND = 'code-actions-sample.command';
+
+/** String to detect in the text document. */
+const EMOJI = 'emoji';
+
+/** Code that is used to associate diagnostic entries with code actions. */
+export const EMOJI_MENTION = 'emoji_mention';
 
 const tokenTypes = new Map<string, number>();
 const tokenModifiers = new Map<string, number>();
@@ -300,4 +305,59 @@ export class Emojinfo implements vscode.CodeActionProvider {
 		action.isPreferred = true;
 		return action;
 	}
+}
+
+
+/**
+ * Analyzes the text document for problems. 
+ * This demo diagnostic problem provider finds all mentions of 'emoji'.
+ * @param doc text document to analyze
+ * @param emojiDiagnostics diagnostic collection
+ */
+export function refreshDiagnostics(doc: vscode.TextDocument, emojiDiagnostics: vscode.DiagnosticCollection): void {
+	const diagnostics: vscode.Diagnostic[] = [];
+
+	for (let lineIndex = 0; lineIndex < doc.lineCount; lineIndex++) {
+		const lineOfText = doc.lineAt(lineIndex);
+		if (lineOfText.text.includes(EMOJI)) {
+			diagnostics.push(createDiagnostic(doc, lineOfText, lineIndex));
+		}
+	}
+
+	emojiDiagnostics.set(doc.uri, diagnostics);
+}
+
+function createDiagnostic(doc: vscode.TextDocument, lineOfText: vscode.TextLine, lineIndex: number): vscode.Diagnostic {
+	// find where in the line of that the 'emoji' is mentioned
+	const index = lineOfText.text.indexOf(EMOJI);
+
+	// create range that represents, where in the document the word is
+	const range = new vscode.Range(lineIndex, index, lineIndex, index + EMOJI.length);
+
+	const diagnostic = new vscode.Diagnostic(range, "When you say 'emoji', do you want to find out more?",
+		vscode.DiagnosticSeverity.Information);
+	diagnostic.code = EMOJI_MENTION;
+	return diagnostic;
+}
+
+export function subscribeToDocumentChanges(context: vscode.ExtensionContext, emojiDiagnostics: vscode.DiagnosticCollection): void {
+	if (vscode.window.activeTextEditor) {
+		refreshDiagnostics(vscode.window.activeTextEditor.document, emojiDiagnostics);
+	}
+	context.subscriptions.push(
+		vscode.window.onDidChangeActiveTextEditor(editor => {
+			if (editor) {
+				refreshDiagnostics(editor.document, emojiDiagnostics);
+			}
+		})
+	);
+
+	context.subscriptions.push(
+		vscode.workspace.onDidChangeTextDocument(e => refreshDiagnostics(e.document, emojiDiagnostics))
+	);
+
+	context.subscriptions.push(
+		vscode.workspace.onDidCloseTextDocument(doc => emojiDiagnostics.delete(doc.uri))
+	);
+
 }
